84cf227e63b29ed90c3307d5ee2f758ea2031431
33616 <33616@DESKTOP-RKER23L.home>
2020-10-22 13:38:18 +0200

    formattage

--- src/etudiants/EtudiantTALInalco.java
+++ src/etudiants/EtudiantTALInalco.java
@@ -14,15 +14,20 @@ public class EtudiantTALInalco extends EtudiantTAL {
 	}
 
 	public String toString() {
-		return "Methode ToString de la classe EtudiantTALInalco. \n"+"Nom =" +nom+"\n" +"langue =" +langue+"\n"+"numeroEtudiant =" +numeroEtudiant+"\n";
+		return "Methode ToString de la classe EtudiantTALInalco. \n" + "Nom =" + nom + "\n" + "langue =" + langue + "\n"
+				+ "numeroEtudiant =" + numeroEtudiant + "\n";
 	}
+
 	public static void main(String[] args) {
 		EtudiantTALInalco etudianteLeonard = new EtudiantTALInalco("Leonard", 32, "anglais");
 		EtudiantTAL etudianteMylene = new EtudiantTAL("Mylene", 22);
-		System.out.println(etudianteLeonard.imprimerID(etudianteLeonard.nom,etudianteLeonard.numeroEtudiant, etudianteLeonard.langue));
+		System.out.println(etudianteLeonard.imprimerID(etudianteLeonard.nom, etudianteLeonard.numeroEtudiant,
+				etudianteLeonard.langue));
 
-		System.out.println(etudianteLeonard.imprimerID(etudianteLeonard.nom,etudianteLeonard.numeroEtudiant, "allemand"));
-		//System.out.println("nom de Bernard="+etudianteLeonard.nom + "age="+etudianteLeonard.age + "langue=" +etudianteLeonard.langue);
+		System.out.println(
+				etudianteLeonard.imprimerID(etudianteLeonard.nom, etudianteLeonard.numeroEtudiant, "allemand"));
+		// System.out.println("nom de Bernard="+etudianteLeonard.nom +
+		// "age="+etudianteLeonard.age + "langue=" +etudianteLeonard.langue);
 
 		System.out.println(etudianteLeonard);
 		System.out.println(etudianteLeonard.toString());
